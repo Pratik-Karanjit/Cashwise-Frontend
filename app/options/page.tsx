4e51022bae65daf747210dd95cd2c9950e03097a
Pratik Karanjit <pratikkaranjit@gmail.com>
2025-08-19 17:19:22 +0545

    split bill and group expenses

--- app/options/page.tsx
+++ app/options/page.tsx
@@ -5,19 +5,7 @@ import Button from "../../components/Button";
 import Image from "next/image";
 import countingCash from "../../public/images/counting.png"
 
-
-
-export default function HeroSection() {
-
-    // const fetchTest = async () => {
-    //     const response = await testMyApi()
-    //     console.log("response iss******", response)
-    // }
-
-    // useEffect(() => {
-    //     fetchTest()
-    // }, [])
-
+export default function Options() {
 
     return (
         <div className='w-full flex bg-white h-[90vh]'>
@@ -25,14 +13,31 @@ export default function HeroSection() {
                 <div className='flex flex-col gap-5'>
 
                     <span className="text-primary text-3xl">Choose your split <span className='text-3xl text-secondary'>option!</span></span>
-                    <div className="mt-5 flex flex-row items-center justify-start gap-2 group cursor-pointer -ml-7">
-                        <FontAwesomeIcon
-                            icon={faArrowRight}
-                            className="inline-block -translate-x-5 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-300 text-secondary"
-                        />
-                        <p className="text-[#5E6282] text-4xl group-hover:text-secondary transition-all duration-300 ">
-                            Split Bill
-                        </p>
+                    <div className="flex flex-col gap-5">
+                        <div className="mt-5 flex flex-row items-center justify-start gap-2 group cursor-pointer -ml-7">
+                            <FontAwesomeIcon
+                                icon={faArrowRight}
+                                className="inline-block -translate-x-5 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-300 text-secondary"
+                            />
+                            <Link href='/splitBill'>
+                                <p className="text-[#5E6282] text-4xl group-hover:text-secondary transition-all duration-300">
+                                    Split Bill
+                                </p>
+                            </Link>
+                        </div>
+
+                        <div className="mt-5 flex flex-row items-center justify-start gap-2 group cursor-pointer -ml-7">
+                            <FontAwesomeIcon
+                                icon={faArrowRight}
+                                className="inline-block -translate-x-5 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-300 text-secondary"
+                            />
+                            <Link href='/groupExpenses'>
+                                <p className="text-[#5E6282] text-4xl group-hover:text-secondary transition-all duration-300">
+                                    Group Expenses
+                                </p>
+                            </Link>
+                        </div>
+
                     </div>
                 </div>
 
